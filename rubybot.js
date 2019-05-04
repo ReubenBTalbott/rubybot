5d501576c73991c94355423d12d37885aa0b3fed
Reuben Talbott <Reuben@talbott.ws>
2019-05-04 16:05:16 -0400

    STOP STEALING MY KEY HACKERS
    people keep stealing my key for my bot and spamming my bots users with ADS! STOP ITS SO ANNOYING

--- rubybot.js
+++ rubybot.js
@@ -7,7 +7,7 @@ var invite = {
 var fs = require('fs');
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.login("MzQ1MjkwMTQzMTIyNDU2NTg1.D1hkog.wgR9cwtpOb8gU7prG-NbHh1NDnc");
+client.login("Your token here");
 client.on('ready', () => {
   client.user.setPresence({ game: { name: 'r.help | ' + client.guilds.size + ' servers', type: 0 } });
   console.log('Ready');
